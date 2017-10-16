2ae546adf45ec5867ebc08060cdb006717903c67
Unknown <wbecerra1@gmail.com>
2017-10-16 17:13:23 +0200

    Exercise 3.4

--- src/graph-plotter/Polynomial.cpp
+++ src/graph-plotter/Polynomial.cpp
@@ -6,9 +6,21 @@ Polynomial::Polynomial(const vector<float> polynomialCoefficients):
 	{}
 
 float Polynomial::evaluate(float x) const{
-    auto sum = 0.f;
-    for(unsigned int index = 0; index != _polynomialCoefficients.size(); index++){
-        sum += _polynomialCoefficients.at(index) * pow(x, _polynomialCoefficients.size() - (index - 1));
+//    auto sum = 0.f;
+//    for(unsigned int index = 0; index != _polynomialCoefficients.size(); index++){
+//        sum += _polynomialCoefficients.at(index) * pow(x, _polynomialCoefficients.size() - (index - 1));
+//    }
+//    return sum;
+        float total = 0;
+    
+    int power = _polynomialCoefficients.size() - 1;
+    
+    for (auto i : _polynomialCoefficients)
+    {
+        total = total + pow((i*x),power);
+        
+        power--;
     }
-    return sum;
+    
+	return total;
 }
--- src/graph-plotter/main.cpp
+++ src/graph-plotter/main.cpp
@@ -9,6 +9,7 @@
 #include "Function.h"
 #include "ExponentialPolynomial.h"
 #include "Polynomial.h"
+#include "Absolute.h"
 
 #include <memory>
 
@@ -30,9 +31,14 @@ int main()
 	float frequency = 1.0;
 	float phase = 0;
 	Sinusoid sine_function{amplitude, frequency, phase};
+    
 	Sinusoid cosine_function{amplitude, frequency, phase + PI/2};
     vector<float> coeffs = {1.0, 2.0, 1.0};
     Polynomial poly_function{coeffs};
+    
+    vector<float> abs_coeffs = {1.0, -3.0, -4.0};
+    auto poly_function_ptr = make_shared<Polynomial> (abs_coeffs);
+    Absolute abs_poly_function (poly_function_ptr);
     
     ExponentialPolynomial expFunction{1.0, 1.5};
 
@@ -53,7 +59,10 @@ int main()
     Range range_new{-3, 1.5};
     graph.plot(generateDataPoints(poly_function, range_new), dot_red);
     graph.plot(generateDataPoints(expFunction, range_new), dot_red);
+    
+    Range range_abs{-4, 7};
+    graph.plot(generateDataPoints(abs_poly_function, range_abs), dot_red);
 
-	return 0;
+    return 0;
 }
 
